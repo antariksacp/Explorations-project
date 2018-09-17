5e22281be20a47274c0ca950f2a68190cdcee529
Carisa Antariksa <antariksacp@gmail.com>
2018-09-17 09:57:25 -0400

    Created grey canvas
    Larger canvas on third hyperlink (ppw-generate)

---
+++ ppw-generate.js
@@ -0,0 +1,17 @@
+var canv
+//var api = "https://api.cognitive.microsoft.com/bing/v7.0/images";
+//var apikey = "&api_key=9e7c6500467f42859c78151a3e7d4e84";
+//var query = "&q=pastel";
+
+function setup(){
+	canv = createCanvas(1000,800);
+	canv.position(300,1000);
+	//var url = api + apikey + query;
+	//loadJSON(url, gotData);
+	input = createInput('');
+
+}
+
+function draw(){
+	background('#dfdee0');
+}
