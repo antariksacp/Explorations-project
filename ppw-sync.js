7c3b86cff45be6b611594282f03baf42ad7dcda6
Carisa Antariksa <antariksacp@gmail.com>
2018-09-17 01:07:48 -0400

    Tag input and image submission
    Added html (with plugin help) for the above features.

---
+++ ppw-sync.js
@@ -0,0 +1,12 @@
+var button;
+
+function setup(){
+	button = createButton("Initializing");
+	button.position(201,300)
+}
+
+function keyPressed(){
+	if(keyCode === RIGHT_ARROW){
+		window.location.href = "file:///C:/Users/caris/OneDrive/Documents/GitHub/Explorations-project/ppw-generate.html"
+	}
+}
